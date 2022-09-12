075debb3a390b28ea4d3cced2e53d1a9e957896d
KPaezAstudillo <karinapaezastudillo@gmail.com>
2022-09-12 19:22:47 +0000

    commit

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const Card = () => {
 	return (
-		<div className="jumbotron m-5">
-		<h1 className="display-4">A Warm Welcome!</h1>
-		<p className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, earum quisquam cum, quia a modi animi maxime dolor eaque quae aliquam, cumque omnis excepturi nesciunt ad aut enim maiores pariatur!</p>
-		<a className="btn btn-primary btn-lg" href="#"role="button">
-			Call to Action
-		</a>
-	</div>
-	);
+		<div className="card container-md col-md-3">
+			<img src="https://picsum.photos/id/237/300" className="card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				</div>
+				<div className="card-footer">
+					<a href="#" className="btn btn-primary ms-auto">Go somewhere</a>
+				</div>
+		</div>	);
 };
 
 export default Card;
